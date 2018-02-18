f80d7cec8b8912f77f7f648ff7cea9e0bcdaacd6
Sebastian Lewandowski <33798697+sebdog@users.noreply.github.com>
2018-02-18 17:25:08 +1030

    added arbitrage opportunities

--- btcArbitrage.py
+++ btcArbitrage.py
@@ -24,16 +24,18 @@ acxData = acxReq.json()
 #print(csData)
 csBuy=csData['prices']['btc']['bid']
 csSell=csData['prices']['btc']['ask']
-
+csBuy=float(csBuy)
+csSell=float(csSell)
 
 #print(bmData)
 bmBuy=bmData['bestBid']
 bmSell=bmData['bestAsk']
 
-
 #print(acxData)
 acxBuy=acxData['btcaud']['ticker']['buy']
 acxSell=acxData['btcaud']['ticker']['sell']
+acxBuy=float(acxBuy)
+acxSell=float(acxSell)
 
 t = PrettyTable(['Exchange','Buy','Sell'])
 t.add_row(['BTC Markets',bmBuy,bmSell])
@@ -41,3 +43,20 @@ t.add_row(['CoinSpot',csBuy,csSell])
 t.add_row(['ACX',acxBuy,acxSell])
 
 print(t)
+
+csFee =0.01	#1%
+bmFee =0.0085	#0.85%
+acxFee=0.002	#0.2%
+
+
+buy=[csBuy,bmBuy,acxBuy]
+sell=[csSell,bmSell,acxSell]
+exchange=['CoinSpot','BTC Markets','ACX']
+
+for x in range(0,3):
+	for y in range(0,3):
+		if x != y:
+			if buy[x]>sell[y]:
+				print 'Buy '+exchange[y]
+				print 'Sell '+exchange[x]+'\n'
+			
